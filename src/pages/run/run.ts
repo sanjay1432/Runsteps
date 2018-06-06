1cfd78f8359adfc420b7dff85f5dbdf29fe6a30e
sanjay <sanjay.d@nextpageit.com>
2018-06-06 16:22:43 +0530

    stepcounter

--- src/pages/run/run.ts
+++ src/pages/run/run.ts
@@ -18,53 +18,54 @@ export class RunPage implements OnInit {
 lat:any;
 long:any;
 distance:number = 0;
+steps;
   constructor(private stepcounter: Stepcounter, public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation) {
   
   }
 
   ngOnInit() {
-    let self = this
-    setInterval(function () {
-      self.getLatLong();
-    }, 1000);
+    // let self = this
+    // setInterval(function () {
+    //   self.getLatLong();
+    // }, 1000);
    
 
   }
 
   getLatLong() { 
-    this.geolocation.getCurrentPosition().then((resp) => {
-      if(this.lat != undefined && this.long != undefined ){
-       let calDis = this.getDistance({lat:resp.coords.latitude,lng:resp.coords.longitude},{lat:this.lat,lng:this.long})
-        console.log(calDis)
-       this.distance += calDis;
-      }
-      this.lat = resp.coords.latitude;
-      this.long = resp.coords.longitude;
+    // this.geolocation.getCurrentPosition().then((resp) => {
+    //   if(this.lat != undefined && this.long != undefined ){
+    //    let calDis = this.getDistance({lat:resp.coords.latitude,lng:resp.coords.longitude},{lat:this.lat,lng:this.long})
+    //     console.log(calDis)
+    //    this.distance += calDis;
+    //   }
+    //   this.lat = resp.coords.latitude;
+    //   this.long = resp.coords.longitude;
 
-     }).catch((error) => {
-       console.log('Error getting location', error);
-    });
+    //  }).catch((error) => {
+    //    console.log('Error getting location', error);
+    // });
     let startingOffset = 0;
-this.stepcounter.start(startingOffset).then(onSuccess => console.log('stepcounter-start success', onSuccess), onFailure => console.log('stepcounter-start error', onFailure));
+this.stepcounter.start(startingOffset).then(onSuccess => this.steps = onSuccess, onFailure => console.log('stepcounter-start error', onFailure));
 
-this.stepcounter.getHistory().then(historyObj => console.log('stepcounter-history success', historyObj), onFailure => console.log('stepcounter-history error', onFailure));
+// this.stepcounter.getHistory().then(historyObj => console.log('stepcounter-history success', historyObj), onFailure => console.log('stepcounter-history error', onFailure));
   }
 
-  rad = (x) => {
-    return x * Math.PI / 180;
-  };
+  // rad = (x) => {
+  //   return x * Math.PI / 180;
+  // };
   
-  getDistance = (p1, p2) => {
-    let R = 6371000; // Earth’s mean radius in meter
-    let dLat = this.rad(p2.lat - p1.lat);
-    let dLong = this.rad(p2.lng - p1.lng);
-    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.rad(p1.lat)) * Math.cos(this.rad(p2.lat)) *
-      Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let d = R * c;
-    return d; // returns the distance in meter
-  };
+  // getDistance = (p1, p2) => {
+  //   let R = 6371000; // Earth’s mean radius in meter
+  //   let dLat = this.rad(p2.lat - p1.lat);
+  //   let dLong = this.rad(p2.lng - p1.lng);
+  //   let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+  //     Math.cos(this.rad(p1.lat)) * Math.cos(this.rad(p2.lat)) *
+  //     Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  //   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  //   let d = R * c;
+  //   return d; // returns the distance in meter
+  // };
   ionViewDidLoad() {
     console.log('ionViewDidLoad RunPage');
   }
